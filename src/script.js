902b7f99ece7d52f72f4d9d26c18e7877c9862f1
Dee <fardee.tat@gmail.com>
2024-02-03 23:45:23 +0700

    -feat: keep detail for navbar

--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
-// alert("Hello")
+const sections = [
+  "profile", "about-me", "education", "activity",
+  "language", "medium", "experience", "skill", "contact-me"
+];
+
+const navigationContaner = document.getElementById("navigation");
+
+sections.forEach(section => {
+  const link = document.createElement("a")
+  link.href = `#${section}`
+  link.className = "font-medium hover:text-blue-700";
+  link.textContent = section.charAt(0).toUpperCase() + section.slice(1).replace("-", " ");
+
+  navigationContaner.appendChild(link)
+})
